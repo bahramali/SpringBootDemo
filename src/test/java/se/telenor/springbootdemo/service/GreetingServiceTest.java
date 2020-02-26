3bb58ae822f88ccb6d781d7f147f74a87cafcf26
azad <bahramali.az@gmail.com>
2020-02-26 20:51:24 +0100

    - Add som test to test handling of exception

--- src/test/java/se/telenor/springbootdemo/service/GreetingServiceTest.java
+++ src/test/java/se/telenor/springbootdemo/service/GreetingServiceTest.java
@@ -2,15 +2,35 @@ package se.telenor.springbootdemo.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import se.telenor.springbootdemo.exception.IdNotNegativeException;
+import se.telenor.springbootdemo.exception.PathIsNotYetImplementedException;
+import se.telenor.springbootdemo.model.Account;
+import se.telenor.springbootdemo.model.Path;
+import se.telenor.springbootdemo.model.Type;
+import se.telenor.springbootdemo.testBuilder.PathTestBuilder;
 
 public class GreetingServiceTest {
 
     GreetingService greetingService = new GreetingServiceImpl();
 
     @Test
-    public void shouldHandelNull(){
+    public void shouldHandelNull() {
         String message = greetingService.getMessage(null);
         Assert.assertNull(message);
     }
+
+    @Test(expected = IdNotNegativeException.class)
+    public void shouldHandelNegativeId() {
+        Path path = PathTestBuilder.builder().withId(-100).withAccountBuilder(Account.BUSINESS).build();
+        greetingService.getMessage(path);
+    }
+
+ @Test(expected = PathIsNotYetImplementedException.class)
+    public void shouldHandelPathIsNotYetImplemented() {
+        Path path = PathTestBuilder.builder().withType(Type.SMALL).withAccountBuilder(Account.BUSINESS).build();
+        greetingService.getMessage(path);
+    }
+
 
 }
